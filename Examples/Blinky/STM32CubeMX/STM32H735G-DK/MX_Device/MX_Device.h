a9e0ab55d7e0ccbb4211556f8b55db82bf29bfae
DavidLesnjak <david.lesnjak@arm.com>
2024-09-18 14:08:19 +0200

    Update Blinky example
    - Update MX_Device.h
    - Remove cdefault.yml
    - Update regions_STM32H743I-EVAL.h
    - Remove "# compiler: AC6" from Blinky.csolution.yml

--- Examples/Blinky/STM32CubeMX/STM32H735G-DK/MX_Device/MX_Device.h
+++ Examples/Blinky/STM32CubeMX/STM32H735G-DK/MX_Device/MX_Device.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File Name   : MX_Device.h
- * Date        : 09/09/2024 12:48:59
+ * Date        : 18/09/2024 14:04:30
  * Description : STM32Cube MX parameter definitions
  * Note        : This file is generated with a generator out of the
  *               STM32CubeMX project and its generated files (DO NOT EDIT!)
@@ -189,6 +189,9 @@
 
 /* Mode */
 #define MX_SDMMC1_MODE_SD                       1
+
+/* Peripheral Clock Frequency */
+#define MX_SDMMC1_PERIPH_CLOCK_FREQ             137500000
 
 /* Pins */
 
